7342dfce755e8b972d6f1b7f424527f6bf2062f6
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-11-06 16:06:40 +0530

    new

--- my-app/src/Components/Pagination.jsx
+++ my-app/src/Components/Pagination.jsx
@@ -4,7 +4,7 @@ function Pagination({arr,current,onChange}) {
   return (
     <div>
       {arr?.map((page)=>{
-        return <button key={page} onClick={()=>onChange(page)} disabled={page===current}>{page}</button>
+        return <button key={page} onClick={()=>onChange(page)} disabled={page===current} style={page===current ? {border:"1px solid red"} : {}}>{page}</button>
       })}
     </div>
   )
